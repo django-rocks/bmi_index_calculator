63d687512ce6a8fe3601ea86d082cbd6a6381347
Andre <aldutoit@hotmail.com>
2022-09-20 15:53:38 +0200

    Changed file to test for move to production

--- app/core/views.py
+++ app/core/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Howzit Broer!!!")
+    return HttpResponse("Howzit Broer!!! - I am going to modify this script.")
