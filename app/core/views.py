09dd2533346cecaeaf936ded731c320e18b85a3b
Andre <aldutoit@hotmail.com>
2022-09-21 19:35:52 +0200

    Passing height and weight using jinja

--- app/core/views.py
+++ app/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def example_view(request):
-    return render(request, 'core/example.html')
+    my_var = {'height':1.87,'weight':120}
+    return render(request, 'core/example.html',context=my_var)
 # def index(request):
 #    return HttpResponse("Howzit")
