d327e2601b774e901cac4d0d2d99cbdd23ce719b
Andre <aldutoit@hotmail.com>
2022-09-21 18:55:56 +0200

    Changed index to example_view in views.py

--- app/core/views.py
+++ app/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
+def example_view(request):
     return render(request, 'core/example.html')
 # def index(request):
 #    return HttpResponse("Howzit")
