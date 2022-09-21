84ff8e434a4cfd6da92a40d41a5a6221a8817c35
Andre <aldutoit@hotmail.com>
2022-09-21 08:38:57 +0200

    Set back to HTTPResponse

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # def example_view(request):
 #    return (request, 'core/example.html')
 def index(request):
-    HttpResponse("Howzit")
+    return HttpResponse("Howzit")
