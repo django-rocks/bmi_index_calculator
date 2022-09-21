72dde5a73853ec558f327326412d46a22663ea61
Andre <aldutoit@hotmail.com>
2022-09-21 09:01:05 +0200

    Changed function name

--- app/core/views.py
+++ app/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# def example_view(request):
-#    return (request, 'core/example.html')
 def index(request):
-    return HttpResponse("Howzit")
+    return (request, 'core/example.html')
+# def index(request):
+#    return HttpResponse("Howzit")
